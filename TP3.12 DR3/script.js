e60b9e5564481420a114e7a1b36216a16062cc9c
Danilo <damilhome@gmail.com>
2024-09-15 12:47:00 -0300

    TP3.12 feito

---
+++ TP3.12 DR3/script.js
@@ -0,0 +1,22 @@
+// - Evento drag
+
+// O evento `drag` ocorre quando um elemento, é `arrastado` pelo usuário. Ele é `disparado` no elemento que está sendo arrastado. Para habilitar o `arrastar` de um elemento, é necessário definir o atributo `draggable`, como `true` para esse elemento. Além disso, é necessário adicionar um `ouvinte de evento`, para o evento `dragstart` no elemento arrastado, que define os dados a serem `transferidos` durante o arrastar, utilizando o método `setData` do objeto `dataTransfer`
+
+// <br>
+
+// - Evento drop
+
+// O evento `drop` ocorre quando um elemento arrastado, é `solto` em um alvo `válido`. Ele é `disparado` no elemento de destino, onde o `elemento arrastado` está sendo `solto`. Para `permitir` que um elemento seja `solto` em um `alvo`, é necessário adicionar um `ouvinte de evento`, para o evento `dragover` no elemento de destino e, dentro desse ouvinte, chamar o método `preventDefault()` para permitir que o elemento seja solto. Além disso, é necessário adicionar um ouvinte de evento para o evento `drop` no elemento de destino, onde a `lógica` para manipular os `dados transferidos` é implementada.
+
+const caixinhas = document.querySelectorAll('.caixinha');
+let manterArquivo;
+caixinhas.forEach(element => element.addEventListener('dragstart', event => manterArquivo = event.target));
+
+const containers = document.querySelectorAll('.container');
+containers.forEach(element => element.addEventListener('dragover', event => event.preventDefault()));
+containers.forEach(element => element.addEventListener('drop', event => {
+    event.preventDefault();
+    if(event.target.className == 'container') {
+        event.target.appendChild(manterArquivo);
+    }
+}));
